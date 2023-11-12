f05d31f9b5d1ee5759ae1b2c6d622f477bf33185
Hanna1991-sys <119236984+Hanna1991-sys@users.noreply.github.com>
2023-11-12 15:04:14 +0200

    first exercise

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+import getRadomHexColor from './randomHexColor';
 
+const dataStartRef = document.querySelector('[data-start]');
+const dataStopRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+
+const timer = null;
+
+dataStartRef.addEventListener('click', onStart);
+dataStopRef.addEventListener('click', onStop);
+
+function onStart() {
+  timerColor = setInterval(getColor, 1000);
+
+  dataStartRef.toggleAttribute('disabled');
+}
+
+function onStop() {
+  clearInterval(timerColor);
+
+  dataStartRef.removeAttribute('disabled');
+}
+
+function getColor() {
+  bodyRef.style.backgroundColor = getRadomHexColor();
+}
