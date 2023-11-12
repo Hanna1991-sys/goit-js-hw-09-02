467cac887a17c957fba9a7cf16ec7e15fb9c806c
Hanna1991-sys <119236984+Hanna1991-sys@users.noreply.github.com>
2023-11-12 13:04:35 +0000

    Deploying to gh-pages from @ Hanna1991-sys/goit-js-hw-09-02@f05d31f9b5d1ee5759ae1b2c6d622f477bf33185 🚀

---
+++ 01-color-switcher.58240485.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),o=document.querySelector("body");function r(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(){timerColor=setInterval(r,1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(timerColor),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.58240485.js.map
